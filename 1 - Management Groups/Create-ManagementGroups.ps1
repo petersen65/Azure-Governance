6c13f8ecabf7c9d3475f7ce26f909144745f9cab
Michael Petersen <mikepet@microsoft.com>
2020-10-14 17:44:13 +0200

    Rework of governance concept

--- 1 - Management Groups/Create-ManagementGroups.ps1
+++ 1 - Management Groups/Create-ManagementGroups.ps1
@@ -7,51 +7,70 @@ $parentId = "/providers/Microsoft.Management/managementGroups/$tenantId"
 $domain = Read-Host -Prompt 'Enter Azure AD Domain Name: '
 
 # Create first level of Management Groups
-New-AzManagementGroup -GroupName 'cloud-projects' -DisplayName 'Cloud Projects' -ParentId $parentId
-New-AzManagementGroup -GroupName 'shared-services' -DisplayName 'Shared Services' -ParentId $parentId
-New-AzManagementGroup -GroupName 'governance-services' -DisplayName 'Governance Services' -ParentId $parentId
+New-AzManagementGroup -GroupName 'custodian-services' -DisplayName 'Custodian Services' -ParentId $parentId
+$division1 = New-AzManagementGroup -GroupName "division-1-projects" -DisplayName "Division 1 Projects" -ParentId $parentId
+$division2 = New-AzManagementGroup -GroupName "division-2-projects" -DisplayName "Division 2 Projects" -ParentId $parentId
 
 # Create project level Management Groups (group names must be unique)
-for (($i = 1), `
-     ($id1 = (Get-AzManagementGroup -GroupName 'cloud-projects').Id), `
-     ($id2 = (Get-AzManagementGroup -GroupName 'shared-services').Id); `
-     $i -le 3; $i++) 
-{ 
-    New-AzManagementGroup -GroupName "cloud-project-$i" -DisplayName "Cloud Project $i" -ParentId $id1 
-    New-AzManagementGroup -GroupName "shared-service-$i" -DisplayName "Shared Service $i" -ParentId $id2 
-}
+New-AzManagementGroup -GroupName "prod-project-x" -DisplayName "PROD Project X" -ParentId $division1
+New-AzManagementGroup -GroupName "uat-project-x" -DisplayName "UAT Project X" -ParentId $division1
+New-AzManagementGroup -GroupName "prod-project-y" -DisplayName "PROD Project Y" -ParentId $division2
+New-AzManagementGroup -GroupName "uat-project-y" -DisplayName "UAT Project Y" -ParentId $division2
 
 # Remove automatically created Owner role assignments on Management Groups
 $ownerId = (Get-AzADUser -UserPrincipalName (Get-AzContext).Account.Id).Id
+Remove-AzRoleAssignment -ObjectId $ownerId -Scope '/providers/Microsoft.Management/managementgroups/custodian-services' -RoleDefinitionName 'Owner'
+Remove-AzRoleAssignment -ObjectId $ownerId -Scope '/providers/Microsoft.Management/managementgroups/division-1-projects' -RoleDefinitionName 'Owner'
+Remove-AzRoleAssignment -ObjectId $ownerId -Scope '/providers/Microsoft.Management/managementgroups/division-2-projects' -RoleDefinitionName 'Owner'
 
-Remove-AzRoleAssignment -ObjectId $ownerId -Scope '/providers/Microsoft.Management/managementgroups/cloud-projects' -RoleDefinitionName 'Owner'
-Remove-AzRoleAssignment -ObjectId $ownerId -Scope '/providers/Microsoft.Management/managementgroups/shared-services' -RoleDefinitionName 'Owner'
-Remove-AzRoleAssignment -ObjectId $ownerId -Scope '/providers/Microsoft.Management/managementgroups/governance-services' -RoleDefinitionName 'Owner'
+Remove-AzRoleAssignment -ObjectId $ownerId -Scope "/providers/Microsoft.Management/managementgroups/prod-project-x" -RoleDefinitionName 'Owner'
+Remove-AzRoleAssignment -ObjectId $ownerId -Scope "/providers/Microsoft.Management/managementgroups/uat-project-x" -RoleDefinitionName 'Owner'
+Remove-AzRoleAssignment -ObjectId $ownerId -Scope "/providers/Microsoft.Management/managementgroups/prod-project-y" -RoleDefinitionName 'Owner'
+Remove-AzRoleAssignment -ObjectId $ownerId -Scope "/providers/Microsoft.Management/managementgroups/uat-project-y" -RoleDefinitionName 'Owner'
 
-for ($i = 1; $i -le 3; $i++) 
-{
-    Remove-AzRoleAssignment -ObjectId $ownerId -Scope "/providers/Microsoft.Management/managementgroups/cloud-project-$i" -RoleDefinitionName 'Owner'
-    Remove-AzRoleAssignment -ObjectId $ownerId -Scope "/providers/Microsoft.Management/managementgroups/shared-service-$i" -RoleDefinitionName 'Owner'
-}
+# Create custodian team identities
+New-AzADGroup -DisplayName 'Custodian Team' -MailNickName 'CT'
+New-AzADUser -DisplayName 'CT User' -UserPrincipalName "ctuser@$domain" -Password (ConvertTo-SecureString 'Passw0rd!#' -AsPlainText -Force) -MailNickname 'CTU'
+Add-AzADGroupMember -MemberObjectId (Get-AzADUser -SearchString 'CT User').Id -TargetGroupObjectId (Get-AzADGroup -SearchString 'Custodian Team').Id
 
-# Create cloud project team identities
-New-AzADGroup -DisplayName 'Cloud Project Team' -MailNickName 'CPT'
-New-AzADUser -DisplayName 'CPT User' -UserPrincipalName "cptuser@$domain" -Password (ConvertTo-SecureString 'Passw0rd!#' -AsPlainText -Force) -MailNickname 'CPTU'
-Add-AzADGroupMember -MemberObjectId (Get-AzADUser -SearchString 'CPT User').Id -TargetGroupObjectId (Get-AzADGroup -SearchString 'Cloud Project Team').Id
+# Create team identities for division 1 projects
+New-AzADGroup -DisplayName 'Division 1 Team' -MailNickName 'D1T'
+New-AzADUser -DisplayName 'D1T User' -UserPrincipalName "d1tuser@$domain" -Password (ConvertTo-SecureString 'Passw0rd!#' -AsPlainText -Force) -MailNickname 'D1TU'
+Add-AzADGroupMember -MemberObjectId (Get-AzADUser -SearchString 'D1T User').Id -TargetGroupObjectId (Get-AzADGroup -SearchString 'Division 1 Team').Id
 
-# Create shared service team identities
-New-AzADGroup -DisplayName 'Shared Service Team' -MailNickName 'SST'
-New-AzADUser -DisplayName 'SST User' -UserPrincipalName "sstuser@$domain" -Password (ConvertTo-SecureString 'Passw0rd!#' -AsPlainText -Force) -MailNickname 'SSTU'
-Add-AzADGroupMember -MemberObjectId (Get-AzADUser -SearchString 'SST User').Id -TargetGroupObjectId (Get-AzADGroup -SearchString 'Shared Service Team').Id
+# Create team identities for project X
+New-AzADGroup -DisplayName 'PROD Project X Team' -MailNickName 'PPXT'
+New-AzADGroup -DisplayName 'UAT Project X Team' -MailNickName 'UPXT'
 
-# Assign cloud project team as Contributor to Management Group
-$cloudProjectTeamId = (Get-AzADGroup -SearchString 'Cloud Project Team').Id
-New-AzRoleAssignment -ObjectId $cloudProjectTeamId -Scope '/providers/Microsoft.Management/managementgroups/cloud-projects' -RoleDefinitionName 'Contributor'
+# Create team identities for division 2 projects
+New-AzADGroup -DisplayName 'Division 2 Team' -MailNickName 'D2T'
+New-AzADUser -DisplayName 'D2T User' -UserPrincipalName "d2tuser@$domain" -Password (ConvertTo-SecureString 'Passw0rd!#' -AsPlainText -Force) -MailNickname 'D2TU'
+Add-AzADGroupMember -MemberObjectId (Get-AzADUser -SearchString 'D2T User').Id -TargetGroupObjectId (Get-AzADGroup -SearchString 'Division 2 Team').Id
 
-# Assign shared services team as Contributor to Management Group
-$sharedServiceTeamId = (Get-AzADGroup -SearchString 'Shared Service Team').Id
-New-AzRoleAssignment -ObjectId $sharedServiceTeamId -Scope '/providers/Microsoft.Management/managementgroups/shared-services' -RoleDefinitionName 'Contributor'
+# Create team identities for project Y
+New-AzADGroup -DisplayName 'PROD Project Y Team' -MailNickName 'PPYT'
+New-AzADGroup -DisplayName 'UAT Project Y Team' -MailNickName 'UPYT'
 
-# Create subscription user identities
-New-AzADUser -DisplayName 'Subscription User 1' -UserPrincipalName "subsuser1@$domain" -Password (ConvertTo-SecureString 'Passw0rd!#' -AsPlainText -Force) -MailNickname 'SUBSU1'
-New-AzADUser -DisplayName 'Subscription User 2' -UserPrincipalName "subsuser2@$domain" -Password (ConvertTo-SecureString 'Passw0rd!#' -AsPlainText -Force) -MailNickname 'SUBSU2'
+# Assign custodian team as Owner to Root Management Group
+$custodianTeamId = (Get-AzADGroup -SearchString 'Custodian Team').Id
+New-AzRoleAssignment -ObjectId $custodianTeamId -Scope "/providers/Microsoft.Management/managementgroups/$parentId" -RoleDefinitionName 'Owner'
+
+# Assign team identities of division 1 projects as Contributor to their Management Group
+$division1TeamId = (Get-AzADGroup -SearchString 'Division 1 Team').Id
+New-AzRoleAssignment -ObjectId $division1TeamId -Scope '/providers/Microsoft.Management/managementgroups/division-1-projects' -RoleDefinitionName 'Contributor'
+
+# Assign team identities of division 2 projects as Contributor to their Management Group
+$division2TeamId = (Get-AzADGroup -SearchString 'Division 2 Team').Id
+New-AzRoleAssignment -ObjectId $division2TeamId -Scope '/providers/Microsoft.Management/managementgroups/division-2-projects' -RoleDefinitionName 'Contributor'
+
+# Assign project X team identities as Contributor to their Management Group
+$prodProjectXTeamId = (Get-AzADGroup -SearchString 'PROD Project X Team').Id
+$uatProjectXTeamId = (Get-AzADGroup -SearchString 'UAT Project X Team').Id
+New-AzRoleAssignment -ObjectId $prodProjectXTeamId -Scope '/providers/Microsoft.Management/managementgroups/prod-project-x' -RoleDefinitionName 'Contributor'
+New-AzRoleAssignment -ObjectId $uatProjectXTeamId -Scope '/providers/Microsoft.Management/managementgroups/uat-project-x' -RoleDefinitionName 'Contributor'
+
+# Assign project Y team identities as Contributor to their Management Group
+$prodProjectYTeamId = (Get-AzADGroup -SearchString 'PROD Project Y Team').Id
+$uatProjectYTeamId = (Get-AzADGroup -SearchString 'UAT Project Y Team').Id
+New-AzRoleAssignment -ObjectId $prodProjectYTeamId -Scope '/providers/Microsoft.Management/managementgroups/prod-project-y' -RoleDefinitionName 'Contributor'
+New-AzRoleAssignment -ObjectId $uatProjectYTeamId -Scope '/providers/Microsoft.Management/managementgroups/uat-project-y' -RoleDefinitionName 'Contributor'
--- 2 - Initiatives/Remove-InitiativesFromManagementGroups.ps1
+++ 2 - Initiatives/Remove-InitiativesFromManagementGroups.ps1
@@ -1,11 +1,3 @@
 # Login to Azure
 Connect-AzAccount
 
-# Remove Policy Set (initiative) definitions and assignments from their scopes
-Remove-AzPolicyAssignment -Name 'cloud-projects' -Scope '/providers/Microsoft.Management/managementgroups/cloud-projects'
-Remove-AzPolicyAssignment -Name 'shared-services' -Scope '/providers/Microsoft.Management/managementgroups/shared-services'
-Remove-AzPolicyAssignment -Name 'governance-services' -Scope '/providers/Microsoft.Management/managementgroups/governance-services'
-
-Remove-AzPolicySetDefinition -Name 'cloud-projects' -ManagementGroupName 'cloud-projects' -Force
-Remove-AzPolicySetDefinition -Name 'shared-services' -ManagementGroupName 'shared-services' -Force
-Remove-AzPolicySetDefinition -Name 'governance-services' -ManagementGroupName 'governance-services' -Force
